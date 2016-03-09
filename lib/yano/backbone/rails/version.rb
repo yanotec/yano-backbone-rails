06f196491c1fc067443c77fe2d9852e2b2627afb
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2016-03-08 23:08:29 -0300

    Upgrade version of backboneJS to 1.3.1

--- lib/yano/backbone/rails/version.rb
+++ lib/yano/backbone/rails/version.rb
@@ -2,7 +2,7 @@ module Yano
   module Backbone
     module Rails
       UNDESCORE_VERSION = "1.8.3"
-      BACKBONE_VERSION = "1.2.3"
+      BACKBONE_VERSION = "1.3.1"
       MARIONETTE_VERSION = "2.4.4"
       VERSION = "0.0.3"
     end
