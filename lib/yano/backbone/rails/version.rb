14bcf39a28b6f9cf3eb70c164a639cc8e23a0a62
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2016-03-08 23:09:25 -0300

    Bumping version 0.0.4

--- lib/yano/backbone/rails/version.rb
+++ lib/yano/backbone/rails/version.rb
@@ -4,7 +4,7 @@ module Yano
       UNDESCORE_VERSION = "1.8.3"
       BACKBONE_VERSION = "1.3.1"
       MARIONETTE_VERSION = "2.4.4"
-      VERSION = "0.0.3"
+      VERSION = "0.0.4"
     end
   end
 end
