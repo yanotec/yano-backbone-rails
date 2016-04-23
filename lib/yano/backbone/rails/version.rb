b3d95119b17bd77dd87476023f372ccd81aaa614
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2016-04-22 21:43:52 -0300

    Update backbone sources

--- lib/yano/backbone/rails/version.rb
+++ lib/yano/backbone/rails/version.rb
@@ -2,8 +2,8 @@ module Yano
   module Backbone
     module Rails
       UNDESCORE_VERSION = "1.8.3"
-      BACKBONE_VERSION = "1.3.1"
-      MARIONETTE_VERSION = "2.4.4"
+      BACKBONE_VERSION = "1.3.3"
+      MARIONETTE_VERSION = "2.4.5"
       VERSION = "0.0.4"
     end
   end
