1e982c917ed0be09a15ccc43ca17672ca87bd0a0
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2016-11-07 17:10:50 -0200

    Bumping version 2.2.2

--- lib/yano/backbone/rails/version.rb
+++ lib/yano/backbone/rails/version.rb
@@ -1,7 +1,7 @@
 module Yano
   module Backbone
     module Rails
-      VERSION = "2.2.1"
+      VERSION = "2.2.2"
       UNDERSCORE_VERSION = "1.8.3"
       BACKBONE_VERSION = "1.3.3"
       MARIONETTE_VERSION = "3.1.0"
