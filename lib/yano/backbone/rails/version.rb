650cf4f8bed2155af24e392b4b83bcf31b40f24e
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2016-11-07 16:03:32 -0200

    Update marionette version and dependencies

--- lib/yano/backbone/rails/version.rb
+++ lib/yano/backbone/rails/version.rb
@@ -4,9 +4,8 @@ module Yano
       VERSION = "2.1.0"
       UNDERSCORE_VERSION = "1.8.3"
       BACKBONE_VERSION = "1.3.3"
-      MARIONETTE_VERSION = "2.4.7"
-      BABYSITTER_VERSION = "0.1.12"
-      RADIO_VERSION = "1.0.5"
+      MARIONETTE_VERSION = "3.0.0"
+      RADIO_VERSION = "2.0.0"
     end
   end
 end
